902d67624e21ae3001f549df6a239360dd918d05
Fengling Hu <hufengling@gmail.com>
2023-02-28 12:21:03 -0500

    Add files via upload
    Improved computational efficiency of hessian3D() bottleneck by ~30-50x.

--- R/hessian3D.R
+++ R/hessian3D.R
@@ -20,64 +20,63 @@
 #' @importFrom pbmcapply pbmclapply
 #' @importFrom pbapply pblapply
 #' @importFrom parallel detectCores
-hessian3D=function(image, mask, radius = 1, parallel = FALSE, cores = 2){
-
+hessian3D = function (image, mask, radius = 1, parallel = FALSE, cores = 2) {
   print("Getting derivatives")
-  grads=gradient3D(image,which="all",radius=radius)
-  gx=grads$Dx
-  gy=grads$Dy
-  gz=grads$Dz
-  rm(grads)
 
-  gradsx=gradient3D(gx,which="all",radius=radius)
-  gxx=gradsx$Dx
-  gxy=gradsx$Dy
-  gxz=gradsx$Dz
-  rm(gx,gradsx)
-
-  gradsy=gradient3D(gy,which="all",radius=radius)
-  gyx=gradsy$Dx
-  gyy=gradsy$Dy
-  gyz=gradsy$Dz
-  rm(gy,gradsy)
-
-  gradsz=gradient3D(gz,which="all",radius=radius)
-  gzx=gradsz$Dx
-  gzy=gradsz$Dy
-  gzz=gradsz$Dz
-  rm(gz,gradsz)
+  grads = gradient3D(image, which = "all", radius = radius)
+  gradsx = gradient3D(grads$Dx, which = "all", radius = radius)
+  gxx = as.vector(gradsx$Dx[mask == 1])
+  gxy = as.vector(gradsx$Dy[mask == 1])
+  gxz = as.vector(gradsx$Dz[mask == 1])
+  # Don't need to independently calculate gyx, gzx, gzy because Hessian is symmetric under smoothness
+  gyy = as.vector(gradient3D(grads$Dy, which = "y", radius = radius)[mask == 1])
+  gyz = as.vector(gradient3D(grads$Dy, which = "z", radius = radius)[mask == 1])
+  gzz = as.vector(gradient3D(grads$Dz, which = "z", radius = radius)[mask == 1])
 
   print("Creating hessian matrices")
-  bigmat=cbind(as.vector(gxx[mask==1]),as.vector(gxy[mask==1]),as.vector(gxz[mask==1]),
-               as.vector(gyx[mask==1]),as.vector(gyy[mask==1]),as.vector(gyz[mask==1]),
-               as.vector(gzx[mask==1]),as.vector(gzy[mask==1]),as.vector(gzz[mask==1]))
+  # Can pre-allocate memory for bigmat (cbind requires matrix copying with each step)
+  mask_voxels = sum(mask)
+  bigmat = matrix(rep(0, 9 * mask_voxels),
+                   nrow = mask_voxels, ncol = 9)
+  bigmat[, 1] = gxx
+  bigmat[, 2] = gxy
+  bigmat[, 3] = gxz
+  bigmat[, 4] = gxy
+  bigmat[, 5] = gyy
+  bigmat[, 6] = gyz
+  bigmat[, 7] = gxz
+  bigmat[, 8] = gyz
+  bigmat[, 9] = gzz
+  rm(grads, gradsx, gxx, gxy, gxz, gyy, gyz, gzz)
+  # garbage collector just in case
+  gc(verbose = FALSE)
 
-  rm(gxx,gxy,gxz,gyx,gyy,gyz,gzx,gzy,gzz)
-
-  biglist=split(bigmat,row(bigmat))
-  biglist=lapply(biglist,matrix,nrow=3,byrow=T)
-
+  # apply() with simplify = F can perform split() + lapply() in one step
+  biglist = apply(bigmat, 1, matrix, nrow = 3, simplify = F)
   rm(bigmat)
+  # garbage collector just in case
+  gc(verbose = FALSE)
 
-  getevals=function(matrix){
-    thiseig=eigen(matrix)$values
-    sort=order(abs(thiseig))
-    return(thiseig[sort])
+  getevals = function(matrix) {
+    # most significant speed-up - Hessian matrix is symmetric (in this case, by design), so can avoid symmetry checks
+    # computationally costly step of eigen() is calculation of eigenvectors, which are unnecessary
+    eigen(matrix, symmetric = TRUE, only.values = TRUE)$values
   }
 
   print("Calculating eigenvalues")
-  if(parallel==TRUE){
-    result=matrix(unlist(pbmclapply(biglist,getevals,mc.cores=cores)),
-                  ncol=3,byrow=T)
-  }else if(parallel==FALSE){
-    result=matrix(unlist(pblapply(biglist,getevals)),ncol=3,byrow=T)
+  if (parallel == TRUE) {
+    result = matrix(unlist(pbmclapply(biglist, getevals, mc.cores = cores)),
+                    nrow = 3) # byrow = T is slower than byrow = F
   }
-  e1=mask
-  e1[mask==1]<-result[,1]
-  e2=mask
-  e2[mask==1]<-result[,2]
-  e3=mask
-  e3[mask==1]<-result[,3]
-
-  return(list(eigval1=e1,eigval2=e2,eigval3=e3))
+  else if (parallel == FALSE) {
+    result = matrix(unlist(pblapply(biglist, getevals)),
+                    nrow = 3) # byrow = T is slower than byrow = F
+  }
+  e1 = mask
+  e1[mask == 1] = result[1, ] # change from columns to row due to above byrow change
+  e2 = mask
+  e2[mask == 1] = result[2, ]
+  e3 = mask
+  e3[mask == 1] = result[3, ]
+  return(list(eigval1 = e1, eigval2 = e2, eigval3 = e3))
 }
